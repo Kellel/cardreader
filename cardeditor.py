fd6f8a67b4936b90a38016b9d6b83d4691b0e88c
Kellen Fox <kellen@cablespeed.com>
2014-10-14 15:11:48 -0700

    the things publishable version

--- cardeditor.py
+++ cardeditor.py
@@ -3,8 +3,6 @@ import signal
 from PyQt4 import QtGui, QtCore
 
 from ui import Ui_Dialog
-from parse_api import Parse
-
 from worker import Worker
 
 class CardEditor(QtGui.QWidget):
@@ -16,28 +14,11 @@ class CardEditor(QtGui.QWidget):
 
         self.device = "/dev/ttyUSB0"
         self.worker = Worker(self.device)
-        self.parse = Parse()
         self.current = None
 
-        # Basic View
-        self.ui.resetButton.clicked.connect(self.reset)
-        self.ui.eraseButton.clicked.connect(self.erase)
-        self.ui.readButton.clicked.connect(self.read)
-        self.ui.writeButton.clicked.connect(self.write)
-        self.ui.writeButton.setEnabled(False)
-        self.ui.lookupButton.clicked.connect(self.basic_user_lookup)
-        self.ui.lookupuserButton.clicked.connect(self.advanced_user_lookup)
-
-        self.ui.lookupEdit.returnPressed.connect(self.advanced_user_lookup)
-        self.ui.userEdit.returnPressed.connect(self.basic_user_lookup)
-
-        self.ui.advancedButton.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(1))
-
         # Advanced View
-        self.ui.basicButton.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(0))
         self.ui.readtracksButton.clicked.connect(self.read_tracks)
         self.ui.writetracksButton.clicked.connect(self.write_tracks)
-        self.ui.lookupuserButton.clicked.connect(self.lookup_user)
 
         self.ui.deviceEdit.setText(self.device)
         self.ui.deviceButton.clicked.connect(self.set_device)
@@ -54,26 +35,15 @@ class CardEditor(QtGui.QWidget):
 
     def write_status(self, msg, msg2 = None, msg3 = None):
         self.ui.statusEdit.moveCursor(QtGui.QTextCursor.End)
-        self.ui.statusEdit_2.moveCursor(QtGui.QTextCursor.End)
 
 
         if msg2 and msg3:
             self.ui.statusEdit.insertPlainText("{}  {}  {}\n".format(msg, msg2, msg3))
-            self.ui.statusEdit_2.insertPlainText("{}  {}  {}\n".format(msg, msg2, msg3))
         else:
             self.ui.statusEdit.insertPlainText(msg + '\n')
-            self.ui.statusEdit_2.insertPlainText(msg + '\n')
 
         self.ui.statusEdit.moveCursor(QtGui.QTextCursor.End)
-        self.ui.statusEdit_2.moveCursor(QtGui.QTextCursor.End)
         self.ui.statusEdit.ensureCursorVisible()
-        self.ui.statusEdit_2.ensureCursorVisible()
-
-    def write_lookup_status(self, msg, msg2, msg3):
-        self.ui.userstatusEdit.moveCursor(QtGui.QTextCursor.End)
-        self.ui.userstatusEdit.insertPlainText("{}  {}  {}\n".format(msg, msg2, msg3))
-        self.ui.userstatusEdit.moveCursor(QtGui.QTextCursor.End)
-        self.ui.userstatusEdit.ensureCursorVisible()
 
     def reset(self):
         self.worker.reset()
@@ -83,56 +53,9 @@ class CardEditor(QtGui.QWidget):
         self.worker.erase()
         self.write_status("[Sent ERASE]")
 
-    def read(self):
-        self.worker.read()
-        self.write_status("[Sent READ]")
-
     def read_tracks(self):
         self.worker.read_tracks()
         self.write_status("[Sent READTRACKS]")
-
-    def basic_user_lookup(self):
-        username = self.ui.userEdit.text().toAscii()
-
-        if username:
-            name, phone, userid = self.lookup_user(username)
-            if name:
-                self.write_status(name, phone, userid)
-                self.ui.userEdit.clear()
-            else:
-                self.write_status("[User {} not found]".format(username))
-                self.ui.userEdit.clear()
-
-    def advanced_user_lookup(self):
-        username = self.ui.lookupEdit.text().toAscii()
-        if username:
-            name, phone, userid = self.lookup_user(username)
-            if name:
-                self.write_lookup_status(name, phone, userid)
-            else:
-                self.write_lookup_status("[User {} not found]".format(username), "", "")
-            self.ui.lookupEdit.clear()
-
-    def lookup_user(self, username):
-        user = self.parse.getUserByUsername(str(username))
-
-        if user:
-            self.current = user
-            self.ui.writeButton.setEnabled(True)
-
-            if 'phone' in user:
-                phone = user['phone']
-            else:
-                phone = '(###)###-####'
-            return user['usernameDisplay'], phone, user['userId']
-        else:
-            return None, None, None
-
-
-    def write(self):
-        self.worker.write(str(self.current['userId']))
-        self.current = None
-        self.ui.writeButton.setEnabled(False)
 
     def write_tracks(self):
         t1 = self.ui.track1Edit.text().toAscii() or None
